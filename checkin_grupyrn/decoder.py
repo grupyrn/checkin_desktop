6d5e7305e738a5cf96898a3986c6903a7a04adb0
Sedir Morais <philippi.sedir@gmail.com>
2018-08-03 03:48:52 -0300

    Support for QRCode decryption

--- checkin_grupyrn/decoder.py
+++ checkin_grupyrn/decoder.py
@@ -1,5 +1,9 @@
 import json
+from cryptography.fernet import Fernet
+from checkin_grupyrn import config
 
 
 def decode_qrcode(qr_data):
-    return json.loads(qr_data)
+    cipher_suite = Fernet(str(config.get('crypto_key')))
+    decrypted_data = cipher_suite.decrypt(str(qr_data))
+    return json.loads(decrypted_data)
