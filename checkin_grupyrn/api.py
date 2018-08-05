a2b73b9f94deb796341f0d0f94896c031d1695ab
Sedir Morais <philippi.sedir@gmail.com>
2018-08-04 22:11:19 -0300

    Dirty fix to avoid empty CPF values

--- checkin_grupyrn/api.py
+++ checkin_grupyrn/api.py
@@ -12,6 +12,13 @@ def current_events():
 
 
 def event_check(member_data, event, check):
+    try:
+        if not member_data.get('cpf'):
+            del member_data['cpf']
+    except Exception:
+        pass
+
     data = {'member': member_data, 'check': check, 'event': event['id']}
     response = requests.post('{}/{}'.format(_base_url, 'check/'), json=data, headers={'Authorization': 'Token ' + _key})
+
     return response.ok, response.json()
