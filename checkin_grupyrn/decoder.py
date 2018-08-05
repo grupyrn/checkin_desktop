0f2fc1b0f0cdfda32f3cc9cba1100d0461b82573
Sedir Morais <philippi.sedir@gmail.com>
2018-08-04 22:13:00 -0300

    Fix for displaying UTF-8 strings on check screen

--- checkin_grupyrn/decoder.py
+++ checkin_grupyrn/decoder.py
@@ -5,5 +5,5 @@ from checkin_grupyrn import config
 
 def decode_qrcode(qr_data):
     cipher_suite = Fernet(str(config.get('crypto_key')))
-    decrypted_data = cipher_suite.decrypt(str(qr_data))
-    return json.loads(decrypted_data)
+    decrypted_data = cipher_suite.decrypt(str(qr_data)).decode('utf-8')
+    return json.loads(decrypted_data, encoding='utf-8')
