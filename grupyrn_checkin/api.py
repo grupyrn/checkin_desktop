4a53c31817ca2644efb6e76cdc751e8a96f7ad24
Sedir Morais <philippi.sedir@gmail.com>
2018-08-29 12:05:47 -0300

    Meet new API changes

--- grupyrn_checkin/api.py
+++ grupyrn_checkin/api.py
@@ -7,18 +7,12 @@ _key = config.get('api_key')
 
 
 def current_events():
-    response = requests.get('{}/{}'.format(_base_url, 'currentevent'), headers={'Authorization': 'Token ' + _key})
+    response = requests.get('{}/{}'.format(_base_url, 'currentevents/'), headers={'Authorization': 'Token ' + _key})
     return response.ok, response.json()
 
 
-def event_check(member_data, event, check):
-    try:
-        if not member_data.get('cpf'):
-            del member_data['cpf']
-    except Exception:
-        pass
-
-    data = {'member': member_data, 'check': check, 'event': event['id']}
+def event_check(uuid, check):
+    data = {'attendee': uuid, 'check': check}
     response = requests.post('{}/{}'.format(_base_url, 'check/'), json=data, headers={'Authorization': 'Token ' + _key})
 
     return response.ok, response.json()
